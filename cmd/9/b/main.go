1ddc8c4d9849694e72775cdd500008aaa24f22a0
Ryan <13948173+rlodge1993@users.noreply.github.com>
2023-12-12 12:06:00 +0000

    Add solution for day 9 part 2

---
+++ cmd/9/b/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	inputBytes, _ := os.ReadFile("input")
+	inputString := strings.TrimSpace(string(inputBytes))
+
+	sequenceStrings := strings.Split(inputString, "\n")
+
+	sequences := [][]int{}
+	for _, sequenceString := range sequenceStrings {
+		numbers := strings.Fields(sequenceString)
+		sequence := []int{}
+		for _, number := range numbers {
+			val, err := strconv.Atoi(number)
+			if err != nil {
+				panic(err)
+			}
+			sequence = append(sequence, val)
+		}
+		sequences = append(sequences, sequence)
+	}
+
+	total := 0
+	for _, sequence := range sequences {
+		total += prevVal(sequence)
+	}
+
+	fmt.Println(total)
+}
+
+func prevVal(sequence []int) int {
+	if allZero(sequence) {
+		return 0
+	}
+
+	diffSeq := []int{}
+	for i := 0; i < len(sequence)-1; i++ {
+		diffSeq = append(diffSeq, sequence[i+1]-sequence[i])
+	}
+
+	return sequence[0] - prevVal(diffSeq)
+}
+
+func allZero(sequence []int) bool {
+	for _, val := range sequence {
+		if val != 0 {
+			return false
+		}
+	}
+	return true
+}
